086d0dd5a1bd748cebbd5805e965faf1ac2f607b
Caleb Fynewever <caleb.fynewever@gmail.com>
2024-12-28 23:29:52 -0600

    build and increase memory setting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Cocoon";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "018b922ddbc055eba57ae263ff5eb2567eaaaeb8";
-  public static final String GIT_DATE = "2024-12-28 17:36:50 EST";
-  public static final String GIT_BRANCH = "cf/2025";
-  public static final String BUILD_DATE = "2024-12-29 00:25:14 EST";
-  public static final long BUILD_UNIX_TIME = 1735449914379L;
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "3d31f13be0660e0d4f103b1c3c083beacb091ebf";
+  public static final String GIT_DATE = "2024-12-29 00:27:00 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-12-29 00:28:59 EST";
+  public static final long BUILD_UNIX_TIME = 1735450139538L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
